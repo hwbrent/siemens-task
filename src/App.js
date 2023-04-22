8f8ed24adcfdd4f1269d2540403da183f874e1c0
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-04-23 00:16:49 +0100

    Increase indentation to 4 spaces

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [items, setItems] = useState([]);
+    const [items, setItems] = useState([]);
 
-  const handleAddItem = () => {
-    const name = prompt("Enter product name:");
-    const date = prompt("Enter use-by date (YYYY-MM-DD):");
-    setItems([...items, { name, date }]);
-  };
+    const handleAddItem = () => {
+        const name = prompt("Enter product name:");
+        const date = prompt("Enter use-by date (YYYY-MM-DD):");
+        setItems([...items, { name, date }]);
+    };
 
-  const handleRemoveItem = (index) => {
-    const newItems = [...items];
-    newItems.splice(index, 1);
-    setItems(newItems);
-  };
+    const handleRemoveItem = (index) => {
+        const newItems = [...items];
+        newItems.splice(index, 1);
+        setItems(newItems);
+    };
 
-  const handleSortItems = () => {
-    const sortedItems = [...items].sort((a, b) => a.date.localeCompare(b.date));
-    setItems(sortedItems);
-  };
+    const handleSortItems = () => {
+        const sortedItems = [...items].sort((a, b) => a.date.localeCompare(b.date));
+        setItems(sortedItems);
+    };
 
-  return (
-    <div>
-      <h1>Food Shopping Products</h1>
-      <button onClick={handleAddItem}>Add Item</button>
-      <button onClick={handleSortItems}>Sort by Use-By Date</button>
-      <ul>
-        {items.map((item, index) => (
-          <li key={index}>
-            {item.name} - {item.date}
-            <button onClick={() => handleRemoveItem(index)}>Remove</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Food Shopping Products</h1>
+            <button onClick={handleAddItem}>Add Item</button>
+            <button onClick={handleSortItems}>Sort by Use-By Date</button>
+            <ul>
+                {items.map((item, index) => (
+                    <li key={index}>
+                        {item.name} - {item.date}
+                        <button onClick={() => handleRemoveItem(index)}>Remove</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
